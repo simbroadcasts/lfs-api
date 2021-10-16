261bbb03c7ab0fab789cb6b1aeb7539553d3c842
pbutcher <pbutcher93@gmail.com>
2021-10-16 19:03:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# lfs-api
+
+Make [Live for Speed](https://lfs.net) JSON API queries in your NodeJS projects.
+
+## Install
+
+Add this module to your project with `npm install lfs-api` or `yarn add lfs-api`
+
+## Usage
+
+Before you can make requests, you need to set up an application on the LFS API.
+
+Once you have set up an application, you can use your `client_id` and `client_secret` to make LFS API calls in your app:
+
+```js
+// Import the LFSAPI module
+import LFSAPI from "lfs-api";
+
+// Create an api class instance
+const api = new LFSAPI("YOUR_CLIENT_API", "YOUR_CLIENT_SECRET");
+
+// Make queries
+const query = await api.makeRequest("vehiclemod");
+```
+
+## API
+
+### `LFSAPI.constructor(client_id, client_secret)`
+
+Create an LFS API instance.
+
+#### Parameters
+
+`client_id` _string_ - Your application's client\*id  
+`client_secret` _string_ - Your application's client_secret
+
+### **`async`** `LFSAPI.makeRequest(endpoint)`
+
+Make an LFS API request.
+
+#### Parameters
+
+`endpoint` _string_ - An LFS API endpoint
+
+### `LFS.setVerbose(verbose)`
+
+Toggle debug logging
+
+`verbose` _boolean_ - Verbose debug messages
---
+++ index.js
@@ -0,0 +1,105 @@
+import fetch from "node-fetch";
+
+/**
+ * @name LFSAPI
+ * @description Make requests to the Live for Speed JSON API
+ * @author Peter Butcher (PButcher) <pete[at]pbutcher[dot]com>
+ * @param {string} client_id LFS API Client ID
+ * @param {string} client_secret LFS API Client Secret
+ */
+class LFSAPI {
+  constructor(client_id, client_secret) {
+    if (typeof client_id !== "string" && typeof client_secret !== "string") {
+      throw new Error(
+        `LFSAPI: Constructor expects 2 arguments of type string (client_id, client_secret), got ${typeof client_id} and ${typeof client_secret} instead.`
+      );
+    }
+
+    // LFS API Version
+    this.version = "0.0.1";
+
+    // LFS API Access Token
+    this.access_token = "";
+
+    // Expire uts
+    this.expires = 0;
+
+    // Verbose
+    this.verbose = false;
+
+    // Client ID
+    this.client_id = client_id;
+
+    // Client Secret
+    this.client_secret = client_secret;
+  }
+
+  // Logging (log, warn, error)
+  _log(msg) {
+    if (this.verbose) console.log(`LFSAPI: ${msg}`);
+  }
+  _warn(msg) {
+    if (this.verbose) console.warn(`LFSAPI Warning: ${msg}`);
+  }
+  _error(msg) {
+    if (this.verbose) console.error(`LFSAPI Error: ${msg}`);
+  }
+
+  // Has the LFS API access token expired?
+  _tokenExpired() {
+    return Date.now() / 1000 > this.expires;
+  }
+
+  // Verbose log messages
+  setVerbose(v) {
+    if (typeof v === "boolean") this.verbose = v;
+    return this;
+  }
+
+  // Make a request
+  async makeRequest(endpoint) {
+    // Get a new access token if the previous one expired
+    if (this._tokenExpired()) {
+      this._log("Renewing Access Token");
+      const params = new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: this.client_id,
+        client_secret: this.client_secret,
+      });
+
+      // Get access token
+      await fetch("https://idtest.liveforspeed.net/oauth2/access_token", {
+        method: "POST",
+        header: "Content-Type: application/x-www-form-urlencoded",
+        body: params,
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          this.access_token = json.access_token;
+          this.expires = Date.now() / 1000 + json.expires_in;
+        })
+        .catch((err) => {
+          this._error(err);
+          return err;
+        });
+    }
+
+    // Make API request
+    return await fetch(`https://apitest.liveforspeed.net/${endpoint}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${this.access_token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        return json;
+      })
+      .catch((err) => {
+        this._error(err);
+        return err;
+      });
+  }
+}
+
+export default LFSAPI;
